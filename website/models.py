19e51e08b33aa7b439957aee3a8d5afa4e5d7f27
AaDalal <57609353+AaDalal@users.noreply.github.com>
2021-09-11 16:05:47 -0400

    Added latitude and longitude columns

--- website/models.py
+++ website/models.py
@@ -8,6 +8,8 @@ class Game(db.Model):
     game_type = db.Column(db.String(100)) # FIXME: this would be better as an enum, but I have no idea how to implement
     num_people = db.Column(db.Integer)
     date = db.Column(db.DateTime(timezone = True), default=func.now())
+    lat = db.Column(db.Float())
+    lng = db.Column(db.Float())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # one to many relationship between user and his/her games
 
     # user.id calls the id field of the user object
