97e6e0ab06de37bbb45c7afa90edf8a82a953710
AaDalal <57609353+AaDalal@users.noreply.github.com>
2021-09-11 14:20:44 -0400

    Updated home to include game creation + updated view / db model

--- website/models.py
+++ website/models.py
@@ -2,16 +2,21 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Note(db.Model):#(things it's inheriting from)
+class Game(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    data = db.Column(db.String(10000))
+    name = db.Column(db.String(100))
+    game_type = db.Column(db.String(100)) # FIXME: this would be better as an enum, but I have no idea how to implement
+    num_people = db.Column(db.Integer)
     date = db.Column(db.DateTime(timezone = True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #one to many relationship between user and his/her notes
-    #user.id calls the id field of the user object
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # one to many relationship between user and his/her games
+
+    # user.id calls the id field of the user object
+
+# -- Add in validation
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique = True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note') #reference to Note - specific to one to many rel
+    games = db.relationship('Game') #reference to Note - specific to one to many rel
