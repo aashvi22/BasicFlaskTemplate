b18a7c697d8b95ab1b52cd6fe4fc48b1a6988019
Aashvi Manakiwala <aashvi22@gmail.com>
2021-09-11 21:49:13 -0400

    pushing views

--- website/views.py
+++ website/views.py
@@ -1,24 +1,25 @@
 #store standard routes for website
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Game
 from . import db
 import json
 
 views = Blueprint('views', __name__)
 
 @views.route('/', methods = ['GET', 'POST'])
-@login_required
+# @login_required
 def home():
     if request.method=='POST':
-        note=request.form.get('note')
-        if len(note)<1:
-            flash('Note too short', category='error')
-        else:
-            new_note = Note(data=note, user_id = current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note created', category='success')
+        address = request.form.get('inputAddress')
+        name = request.form.get('gameName')
+        sportSelect = request.form.get('sportSelect')
+        numPeople = request.form.get('numPeople')
+        makePublic = request.form.get('makePublic', type = bool)
+        new_game = Game(name = name, game_type = sportSelect, num_people = numPeople)
+        db.session.add(new_game)
+        db.session.commit()
+        flash('Game created', category='success')
     return render_template("home.html", user=current_user)
 
 @views.route('/delete-note', methods=['POST'])
